56f304d640e6b849de8a3e42577f9a1a60724a23
Dmitry Shishkin <4144442@bk.ru>
2022-04-14 17:55:08 +0300

    CRUD_1 is finished

--- src/main/java/ru/alishev/springcourse/dao/PersonDAO.java
+++ src/main/java/ru/alishev/springcourse/dao/PersonDAO.java
@@ -32,4 +32,13 @@ public class PersonDAO {
         person.setId(++PEOPLE_COUNT);
         people.add(person);
     }
+
+    public void update(int id, Person updatedPerson) {
+        Person personToUpdated = show(id);
+        personToUpdated.setName(updatedPerson.getName());
+    }
+
+    public void delete(int id) {
+        people.removeIf(person -> person.getId() == id);
+    }
 }
